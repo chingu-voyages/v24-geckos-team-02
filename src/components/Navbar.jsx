c22786d4f6ae12728a5077af37aaf3f93a47d640
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-19 00:07:50 +0100

    add text logo, remove text-decoration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import '../css/Navbar.css'
 
 export default function Navbar() {
   return (
     <nav>
-      <div className="logo">BOOKS PLUS</div>
+      <div className="logo">BOOKS<span>PLUS</span></div>
       <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">Team</a></li>
