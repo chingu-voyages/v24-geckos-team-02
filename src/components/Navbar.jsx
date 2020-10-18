50a01f6bcc67e936ed5b9924cf448afd73cc07a8
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-18 23:19:53 +0100

    add basic layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Navbar() {
-  return <nav>*To insert Logo and shorcuts here*</nav>;
+  return (
+    <nav>
+      <div className="logo">BOOKS PLUS</div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Contacts</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </nav>
+  )
 }
