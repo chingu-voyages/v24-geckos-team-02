8c6a66b91e5275a0ab287431e111a5b046bfdd38
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-21 13:36:17 +0100

    add logo - refactor to only show logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import '../css/Navbar.css'
 
 export default function Navbar() {
+  const styles = {
+    height: 80,
+    padding: 10
+  }
+
   return (
     <nav>
-      <div className="logo">BOOKS<span>PLUS</span></div>
+      <img src={require("../img/books-plus-v1.svg")} style={styles} alt="logo" />
       <ul className="nav-links">
         {/* <li><a href="#">Home</a></li>
         <li><a href="#">Team</a></li>
