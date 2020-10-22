67001c92593b741a95f4aaedd4f7bfe4fa7be252
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-22 10:01:23 +0100

    create assets/img folder in src, update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import '../css/Navbar.css'
 
 export default function Navbar() {
   const styles = {
-    height: 80,
+    height: 100,
     padding: 10
   }
 
   return (
     <nav>
-      <img src={require("../img/books-plus-v1.svg")} style={styles} alt="logo" />
+      <img src={require("../assets/img/logo.svg")} style={styles} alt="logo" />
       <ul className="nav-links">
         {/* <li><a href="#">Home</a></li>
         <li><a href="#">Team</a></li>
