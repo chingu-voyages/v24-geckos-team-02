d888f76a65f58756faa0c3660b5ecee79bbd41b6
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-30 03:11:23 +0000

    add href links to icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <footer className="footer bg-dark">
       <div class="social">
-        <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-        <a href="#"><i class="fab fa-youtube fa-2x"></i></a>
+        <a target='_blank' href="https://twitter.com/ChinguCollabs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter fa-2x"></i></a>
+        <a target='_blank' href="https://www.youtube.com/channel/UCS7zmJXbe7FgTC3sHlUf4jw"><i class="fab fa-youtube fa-2x"></i></a>
       </div>
       <p>Copyright &copy; 2020 - BooksPlus</p>
     </footer>
