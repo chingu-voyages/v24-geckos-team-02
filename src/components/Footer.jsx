959b36a260ce9dd2fc9bd0cd52688078ca060533
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-30 02:39:54 +0000

    add font-awsome link and icons, copyright title, styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import '../css/Footer.css'
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer bg-dark">
+      <div class="social">
+        <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+        <a href="#"><i class="fab fa-youtube fa-2x"></i></a>
+      </div>
       <p>Copyright &copy; 2020 - BooksPlus</p>
     </footer>
   );
