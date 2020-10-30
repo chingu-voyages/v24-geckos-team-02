a9afc352d88c963d0da1ab5caa524ce11247b079
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-30 00:11:30 +0000

    update logo img to import method, resize logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import logo from '../assets/img/logo.svg'
 import '../css/Navbar.css'
 
 export default function Navbar() {
   const styles = {
-    height: 100,
+    height: 80,
     padding: 10
   }
 
   return (
     <nav>
-      <img src={require("../assets/img/logo.svg")} style={styles} alt="logo" />
+      <img src={logo} style={styles} alt="logo" />
       <ul className="nav-links">
         <li><a target='_blank' href="https://github.com/chingu-voyages/v24-geckos-team-02/graphs/contributors">About</a></li>
       </ul>
