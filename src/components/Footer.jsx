cddf214d479c01ed744324743e76de8f13b202c7
Guitarhub786 <codewithdrawal@gmail.com>
2020-10-31 19:02:43 +0000

    add logo and change style to row to reduce height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React from "react"
+import logo from '../assets/img/logo.svg'
 import '../css/Footer.css'
 
 export default function Footer() {
+  const styles = {
+    height: 70,
+    padding: 10
+  }
+
   return (
     <footer className="footer bg-dark">
+      <img src={logo} style={styles} alt="logo" />
       <div class="social">
         <a target='_blank' href="https://twitter.com/ChinguCollabs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter fa-2x"></i></a>
         <a target='_blank' href="https://www.youtube.com/channel/UCS7zmJXbe7FgTC3sHlUf4jw"><i class="fab fa-youtube fa-2x"></i></a>
       </div>
       <p>Copyright &copy; 2020 - BooksPlus</p>
+
+      {/* <div class="social">
+        <a target='_blank' href="https://twitter.com/ChinguCollabs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter fa-2x"></i></a>
+        <a target='_blank' href="https://www.youtube.com/channel/UCS7zmJXbe7FgTC3sHlUf4jw"><i class="fab fa-youtube fa-2x"></i></a>
+      </div>
+      <p>Copyright &copy; 2020 - BooksPlus</p> */}
     </footer>
   );
 }
