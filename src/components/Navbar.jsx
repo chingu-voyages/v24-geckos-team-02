4147f0aae722321dab61827be6494b565766edd5
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-11-02 01:36:11 +0530

    Implement login and logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,59 @@
-import React from "react";
-import logo from '../assets/img/logo.svg'
-import '../css/Navbar.css'
+import React, { useState } from "react";
+import logo from "../assets/img/logo.svg";
+import "../css/Navbar.css";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 export default function Navbar() {
+  const [name, setName] = useState("");
+
   const styles = {
     height: 70,
-    padding: 10
-  }
+    padding: 10,
+  };
+
+  const loginFailure = (response) => {
+    console.log(response);
+    document.alert("Login failed");
+  };
+
+  const loginSuccess = (response) => {
+    console.log(response);
+    setName(response.profileObj.name);
+  };
+
+  const logoutSuccess = (response) => {
+    console.log("logged out");
+    setName("");
+  };
 
   return (
     <nav>
       <img src={logo} style={styles} alt="logo" />
       <ul className="nav-links">
-        <li><a target='_blank' href="https://github.com/chingu-voyages/v24-geckos-team-02/graphs/contributors">About</a></li>
+        <li>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/chingu-voyages/v24-geckos-team-02/graphs/contributors">
+            About
+          </a>
+        </li>
+        <li>
+          {!name ? (
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              buttonText="Login"
+              onSuccess={loginSuccess}
+              onFailure={loginFailure}
+              cookiePolicy={"single_host_origin"}
+            />
+          ) : (
+            <div style={{ display: "flex" }}>
+              <p>{name}</p>
+              <GoogleLogout clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} buttonText="Logout" onLogoutSuccess={logoutSuccess}>
+                Logout
+              </GoogleLogout>
+            </div>
+          )}
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
