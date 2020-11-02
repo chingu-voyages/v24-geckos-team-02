25279a3aa6da0c9f76950b46525574eb370e1123
Raphael Delorme <mokokebeloke@gmail.com>
2020-11-02 14:30:43 +0100

    Removed buy link when price isn't available

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,56 +1,72 @@
-import React from "react"
-import formatLanguage from "../utils/isoLang"
-import formatDate from "../utils/formatDate"
+import React from "react";
+import formatLanguage from "../utils/isoLang";
+import formatDate from "../utils/formatDate";
 
-export default function Modal({book, toggleModal}) {
-  const {
-    title, 
-    averageRating, 
-    ratingsCount, 
-    categories, 
-    publishedDate, 
-    description, 
-    previewLink, 
-    infoLink, 
-    buyLink, 
-    retailPrice,
-    pageCount,
-    language,
-    thumbnailImageLink
-  } = book
+export default function Modal({ book, toggleModal }) {
+	const {
+		title,
+		averageRating,
+		ratingsCount,
+		categories,
+		publishedDate,
+		description,
+		previewLink,
+		infoLink,
+		buyLink,
+		retailPrice,
+		pageCount,
+		language,
+		thumbnailImageLink,
+	} = book;
 
-  let readablePublishedDate = formatDate(publishedDate)
+	let readablePublishedDate = formatDate(publishedDate);
 
-  let formattedLanguage = formatLanguage(language);
+	let formattedLanguage = formatLanguage(language);
 
-  const style = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "white"
-  }
+	const style = {
+		position: "fixed",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%, -50%)",
+		backgroundColor: "white",
+	};
+	console.log(retailPrice.amount);
 
-  return(
-    <div className="modal-container" style={style}>
-      <h3>{title}</h3>
-      <img
-        className="cover"
-        src={thumbnailImageLink}
-        alt="cover thumbnail"
-      />
-      {/* change rating by stars? */}
-      <span>{averageRating} / 5 ({ratingsCount} {ratingsCount > 1 ? "votes" : "vote"})</span>
-      <p>{categories}</p>
-      <p>Published date: {readablePublishedDate}</p>
-      <p>{description}</p>
-      <p>{pageCount} pages, language: {formattedLanguage}</p>
-      <p><a href={previewLink} target="blank" >preview</a></p>
-      <p><a href={infoLink} target="blank" >info</a></p>
-      <p><a href={buyLink} target="blank" >buy for {retailPrice.amount}{retailPrice.currencyCode}</a></p>
-      
-      <button onClick={()=>toggleModal()}>close</button>
-    </div>
-  )
-};
+	return (
+		<div className="modal-container" style={style}>
+			<h3>{title}</h3>
+			<img className="cover" src={thumbnailImageLink} alt="cover thumbnail" />
+			{/* change rating by stars? */}
+			<span>
+				{averageRating} / 5 ({ratingsCount}{" "}
+				{ratingsCount > 1 ? "votes" : "vote"})
+			</span>
+			<p>{categories}</p>
+			<p>Published date: {readablePublishedDate}</p>
+			<p>{description}</p>
+			<p>
+				{pageCount} pages, language: {formattedLanguage}
+			</p>
+			<p>
+				<a href={previewLink} target="blank">
+					preview
+				</a>
+			</p>
+			<p>
+				<a href={infoLink} target="blank">
+					info
+				</a>
+			</p>
+			{retailPrice.amount ? (
+				<p>
+					<a href={buyLink} target="blank">
+						buy for {retailPrice.amount}
+						{retailPrice.currencyCode}
+					</a>
+				</p>
+			) : null}
 
+			<button onClick={() => toggleModal()}>close</button>
+		</div>
+	);
+}
