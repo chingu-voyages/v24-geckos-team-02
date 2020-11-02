0cb57c6e4a8ad751eb69ca3e573d68ce91bc2dbc
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-11-02 15:08:09 +0530

    Fix: change more instances of class to className in jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,10 +17,10 @@ export default function Footer() {
           rel="noopener noreferrer"
           href="https://twitter.com/ChinguCollabs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
         >
-          <i class="fab fa-twitter fa-2x"></i>
+          <i className="fab fa-twitter fa-2x"></i>
         </a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCS7zmJXbe7FgTC3sHlUf4jw">
-          <i class="fab fa-youtube fa-2x"></i>
+          <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
       <p>Copyright &copy; 2020 - BooksPlus</p>
