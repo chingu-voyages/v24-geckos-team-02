1a3c2b20e350ee48fa85a234574fe0ddbf4f30c0
snrelgh se <snrelghmail@gmail.com>
2020-11-03 20:42:23 +1000

    import some code/logic from Navbar.css into App.scss & fix various issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import "../css/Navbar.css";
 
 export default function Navbar() {
   const styles = {
-    height: 100,
-    padding: 10,
+    height: 95,
+    padding: 15,
   };
 
   return (
@@ -14,11 +14,11 @@ export default function Navbar() {
       <div className="nav-logo">
       <img src={logo} style={styles} alt="logo" />
       </div>
-      <div className="nav-list">
-        <ul className="nav-list-items">
+      {/* <div className="nav-list"> */}
+        <ul className="nav-list">
             <li className="nav-link"><a target='_blank' href="https://github.com/chingu-voyages/v24-geckos-team-02/graphs/contributors">About</a></li>
         </ul>
-      </div>
+      {/* </div> */}
     </nav>
   );
 }
