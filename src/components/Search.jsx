4c1b42665e75b9455d60aa2b35c48cd863d95b4c
snrelgh se <snrelghmail@gmail.com>
2020-11-05 21:09:12 +1000

    fix alignment issue with error message

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ export default function Search({ handleSubmit, error, queryHistory }) {
   return (
     <div className="search-section">
       <SearchBar handleSubmit={handleSubmit} queryHistory={queryHistory} />
-      <div className="search-error" style={{ color: "red" }}>
+      <div className="search-error" style={{ position:"relative", marginTop:"5px", textAlign: "center", backgroundColor: "white", color: "#B30000" }}>
         {error && error}
       </div>
     </div>
