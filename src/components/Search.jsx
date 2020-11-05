27dc024407b160ec4685f7d424cd45e66e477517
snrelgh se <snrelghmail@gmail.com>
2020-11-05 21:25:17 +1000

    refactor code: create new object to include err msg styling info

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
+let errorMsgStyle = {
+  position: "relative",
+  marginTop: "5px",
+  padding: "2px",
+  textAlign: "center",
+  backgroundColor: "white",
+  color: "#B30000",
+};
+
 export default function Search({ handleSubmit, error, queryHistory }) {
   return (
     <div className="search-section">
       <SearchBar handleSubmit={handleSubmit} queryHistory={queryHistory} />
-      <div className="search-error" style={{ position:"relative", marginTop:"5px", textAlign: "center", backgroundColor: "white", color: "#B30000" }}>
+      <div className="search-error" style={errorMsgStyle}>
         {error && error}
       </div>
     </div>
