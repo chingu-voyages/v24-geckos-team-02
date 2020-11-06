648ea7b55d4ea37118f926bd6df69d0a9eda504e
Guitarhub786 <codewithdrawal@gmail.com>
2020-11-06 21:05:38 +0000

    delete comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-//import logo from "../assets/img/logo.svg";
 import "../css/Footer.css";
 
 export default function Footer() {
-  // const styles = {
-  //   height: 70,
-  //   padding: 10,
-  // };
 
   return (
     <footer className="footer-section">
@@ -17,12 +12,6 @@ export default function Footer() {
         <a target='_blank' className="footer-link" href="https://github.com/chingu-voyages/v24-geckos-team-02"><i class="fab fa-github fa-2x"></i></a>
       </div>
       <p className="footer-note">Copyright &copy; 2020 - BooksPlus</p>
-
-      {/* <div class="footer-section">
-        <a target='_blank' href="https://twitter.com/ChinguCollabs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter fa-2x"></i></a>
-        <a target='_blank' href="https://www.youtube.com/channel/UCS7zmJXbe7FgTC3sHlUf4jw"><i class="fab fa-youtube fa-2x"></i></a>
-      </div>
-      <p>Copyright &copy; 2020 - BooksPlus</p> */}
     </footer>
   );
 }
