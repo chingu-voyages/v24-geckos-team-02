81f19ed1601ee835662b0b43f343ebbaeb4aa3c0
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-11-08 18:42:30 +0530

    Fix styles in Favorites.jsx

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Favorites() {
-  return <div>Favorite page</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div>Favorites</div>
+      <Footer />
+    </div>
+  );
 }
