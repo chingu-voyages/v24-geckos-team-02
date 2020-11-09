a1ff133d37b9f0eb558a9c5cdd8e5659d8ce5884
Guitarhub786 <codewithdrawal@gmail.com>
2020-11-09 15:49:23 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# v24-geckos-team-02
+# Books Plus
+
+## v24-geckos-team-02
 Books Plus | Voyage-24 | https://chingu.io/
 
 Netlify | booksplus-beta | https://booksplus-beta.netlify.app/
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,51 +5,51 @@ import Modal from "./Modal";
 // functional component responsible to render one individual Card aka Volume (Book, Magazine or Newspaper)
 export default function Card({ book, query }) {
   const [showModal, setshowModal] = useState(false);
-  const {thumbnailImageLink, title, subtitle, authors, categories, publisher } = book
+  const { thumbnailImageLink, title, subtitle, authors, categories, publisher } = book
 
   useEffect(() => {
     setshowModal(false)
   }, [query])
-  
+
 
   const toggleModal = () => {
-    setshowModal(prev=>!prev)
+    setshowModal(prev => !prev)
   }
 
   const style = {
-      position: "fixed",
-      top: "0",
-      left: "0",
-      width: "100%",
-      height: "100%",
-      background: "rgba(0, 0, 0, 0.6)",
-      display: showModal ? "block" : "none"
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.6)",
+    display: showModal ? "block" : "none"
   }
 
   return (
-<>
-<div className="modal-overlay" style={style} onClick={toggleModal}></div>
-    <div className="card">
-      <div className="card-left">
-        <img
-          className="card-image"
-          src={thumbnailImageLink}
-          alt="cover thumbnail"
-        />
+    <>
+      <div className="modal-overlay" style={style} onClick={toggleModal}></div>
+      <div className="card">
+        <div className="card-left">
+          <img
+            className="card-image"
+            src={"https://www.google.com/search?q=image+no+available&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiArZuk9ZfrAhUBuaQKHSUxCkIQ_AUoAXoECAwQAw&biw=2560&bih=1258#imgrc=c9HhSMkB_LemLM&imgdii=bjitzqYIP9hbJM"}
+            alt="cover thumbnail"
+          />
+        </div>
+        <div className="card-right">
+          <h2 className="card-title">{title}</h2>
+          <h3 className="card-subtitle">{subtitle}</h3>
+          <p className="card-authors">By: {authors.join(", ")}</p>
+          {/* <p className="card-authors">{categories.join(", ")}</p> */}
+          <p className="card-publisher">Published by: {publisher}</p>
+          <button className="card-btn" onClick={() => toggleModal()}>{showModal ? "close" : "more details"}</button>
+        </div>
       </div>
-      <div className="card-right">
-        <h2 className="card-title">{title}</h2>
-        <h3 className="card-subtitle">{subtitle}</h3>
-        <p className="card-authors">By: {authors.join(", ")}</p>
-        {/* <p className="card-authors">{categories.join(", ")}</p> */}
-        <p className="card-publisher">Published by: {publisher}</p>
-        <button className="card-btn" onClick={()=>toggleModal()}>{showModal ? "close" : "more details"}</button>
-      </div>
-    </div>
       {showModal ? <Modal book={book} toggleModal={toggleModal} /> : null}
- 
 
-</>
+
+    </>
   );
 }
 
