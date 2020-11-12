c1c17526941264ea7fb5db107d5361b61601c19f
snrelgh se <snrelghmail@gmail.com>
2020-11-12 17:29:10 +1000

    align card btns to the left of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -120,7 +120,7 @@ export default function Card({
           <div
             style={{
               display: "flex",
-              justifyContent: "space-around",
+              //justifyContent: "space-around",
               width: "100%",
             }}
           >
