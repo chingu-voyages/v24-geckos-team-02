ef24b434188ad8bcdc9972028dd672d1a8484ffc
snrelgh se <snrelghmail@gmail.com>
2020-11-12 16:51:18 +1000

    add new search option to menu options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,9 @@ export default function Navbar(props) {
             <div id="bar3" className="bar"></div>
           </div>
           <ul className="nav" id="nav">
+            <Link to="/">
+            <li className="nav-link">Search</li>
+            </Link>
             <li><a target='_blank' href="https://github.com/chingu-voyages/v24-geckos-team-02/graphs/contributors">About</a></li>
             <li><Authentication {...props} /></li>
           </ul>
