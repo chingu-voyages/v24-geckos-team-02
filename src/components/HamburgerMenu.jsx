c4ebb3a98fd65bd0469f3663d653507da0a18cf0
snrelgh se <snrelghmail@gmail.com>
2020-11-15 07:34:33 +1000

    allow user to off click & more conveniently close hamburger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -4,16 +4,34 @@ import { Link } from "react-router-dom";
 import Authentication from "./Authentication";
 
 const HamburgerMenu = (props) => {
-    let displayOptions = () => {
-        document.getElementById("menu").classList.toggle("change");
-        document.getElementById("nav").classList.toggle("change-menu-only");
     
-      }
+    //helper function to handle routine operations associated with opening & closing menu 
+    const closeOrOpenMenu = (menu, menuOptions) => {
+      menu.classList.toggle("change");
+      menuOptions.classList.toggle("change-menu-options-only");
+    };
+
+    const handleMenuClick = () => {
+      const menu = document.getElementById("menu");
+      const menuOptions = document.getElementById("nav");
+
+      const handleOffClick = () => {
+        closeOrOpenMenu(menu, menuOptions); //close menu options on an off click aka toggle everything
+        document.removeEventListener("click", handleOffClick); //safely remove Event Listener on an 'off click'
+      };
+
+      closeOrOpenMenu(menu, menuOptions);  //open menu options on click by default 
+
+      //if menu options are opened, add an Event Listener to document & handle potential off click
+      menuOptions.className === "nav change-menu-options-only"
+        ? document.addEventListener("click", handleOffClick)
+        : closeOrOpenMenu(menu, menuOptions);
+    };
 
     return (
         <>
         <div id="menu-bar">
-        <div id="menu" onClick={displayOptions}>
+        <div id="menu" onClick={handleMenuClick}>
           <div id="bar1" className="bar"></div>
           <div id="bar2" className="bar"></div>
           <div id="bar3" className="bar"></div>
